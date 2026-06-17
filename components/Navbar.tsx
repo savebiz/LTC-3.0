3e0ba0b74d6a36fe41abd37d4d3dbdbea3fd5bdc
savebiz <sabo.victor1@gmail.com>
2026-06-17 13:28:03 +0100

    style: adjust desktop scrolled divider spacing (12px left / 6px right)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -74,7 +74,7 @@ const Navbar: React.FC<NavbarProps> = ({ onRegisterClick }) => {
             </div>
             <div className={`w-[1px] transition-all duration-300 ${
               isScrolled 
-                ? 'h-[20px] bg-zinc-700/30 mx-3' 
+                ? 'h-[20px] bg-zinc-700/30 ml-3 mr-[6px]' 
                 : 'h-[30px] bg-white/30 ml-4 mr-2'
             }`} />
             <img
